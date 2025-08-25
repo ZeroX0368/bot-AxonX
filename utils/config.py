dc6c39ed3c88c7c8b6159c9cac9e7eb192b05161
ZeroX0368 <dabibanban@gmail.com>
2025-08-25 13:37:16 +0700

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,9 +1,9 @@
 import os
 
 TOKEN = os.environ.get("TOKEN")
-NAME = "Axon X"
-server = "https://discord.com/invite/codexdev"
-ch = "https://discord.com/channels/699587669059174461/1271825678710476911"
+NAME = "Avalon X"
+server = "https://discord.gg/Zg2XkS5hq9"
+ch = "https://discord.com/channels/1385273333720940778/1393807816937963636"
 OWNER_IDS = [1142053791781355561]
-BotName = "GhostX"
-serverLink = "https://discord.com/invite/codexdev"
+BotName = "Avalon X"
+serverLink = "https://discord.gg/Zg2XkS5hq9"
